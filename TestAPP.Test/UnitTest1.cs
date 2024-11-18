d606e421ee29109b4830321e64000725b4bba5b9
Lili <lili.berenyi@gmail.com>
2024-11-18 12:33:34 +0000

    Add tasks 4-5, without using Assert Multiple

--- TestAPP.Test/UnitTest1.cs
+++ TestAPP.Test/UnitTest1.cs
@@ -12,22 +12,32 @@ namespace TestAPP.Test
         }
 
         [Test]
-        [TestCase(Point.NORTH, Direction.LEFT, Point.SOUTH, TestName = "TestRoatate")]
+        [TestCase(Point.NORTH, Direction.RIGHT, Point.EAST, TestName = "Test Roatate: North, right => EAST")]
+        [TestCase(Point.NORTH, Direction.LEFT, Point.WEST, TestName = "Test Roatate: North, left => WEST")]
+        [TestCase(Point.EAST, Direction.RIGHT, Point.SOUTH, TestName = "Test Roatate: East, right => SOUTH")]
+        [TestCase(Point.EAST, Direction.LEFT, Point.NORTH, TestName = "Test Roatate: East, left => NORTH")]
+        [TestCase(Point.SOUTH, Direction.RIGHT, Point.WEST, TestName = "Test Roatate: South, right => WEST")]
+        [TestCase(Point.SOUTH, Direction.LEFT, Point.EAST, TestName = "Test Roatate: South, left => EAST")]
+        [TestCase(Point.WEST, Direction.RIGHT, Point.NORTH, TestName = "Test Roatate: West, right => NORTH")]
+        [TestCase(Point.WEST, Direction.LEFT, Point.SOUTH, TestName = "Test Roatate: West, left => SOUTH")]
+
+
         [Description(" returns EAST when passed NORTH and RIGHT")]
         public void TestRotate(Point inputPoint, Direction inputDirection, Point expectedResult)
         {
             //Arrange
-            //Point point = Point.NORTH;
-            //Direction direction = Direction.LEFT;
-            //Point expectedResult = Point.EAST;
             Compass compass = new Compass();
 
             //Act
-           Point result = compass.Rotate(inputPoint, inputDirection);
+            Point result = compass.Rotate(inputPoint, inputDirection);
 
             //Assert
-            result.Should().Be(expectedResult);  
-           
+            result.Should().Be(expectedResult);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(expectedResult , Is.EqualTo(result));
+            });
         }
        
 
--- TestApp/Compass.cs
+++ TestApp/Compass.cs
@@ -11,7 +11,28 @@ namespace TestApp
         public Point point {  get; set; }
         public Point Rotate(Point inputPoint, Direction inputDirection)
         {
-            return Point.SOUTH;
+            if (inputPoint == Point.NORTH)
+            {
+                if (inputDirection == Direction.RIGHT) return Point.EAST;
+                else return Point.WEST;
+            }
+            if (inputPoint == Point.EAST)
+            {
+                if (inputDirection == Direction.RIGHT) return Point.SOUTH;
+                else return Point.NORTH;
+            }
+            if (inputPoint == Point.SOUTH)
+            {
+                if (inputDirection == Direction.RIGHT) return Point.WEST;
+                else return Point.EAST;
+            }
+            if (inputPoint == Point.WEST)
+            {
+                if (inputDirection == Direction.RIGHT) return Point.NORTH;
+                else return Point.SOUTH;
+            }
+            return Point.NORTH;
+
         }
     }
 }
