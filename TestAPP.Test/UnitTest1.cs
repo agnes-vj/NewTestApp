dab5309c2adac7610521e9e809036a3a030c60f4
Agnes <133767448+agnes-vj@users.noreply.github.com>
2024-11-18 16:31:17 +0000

    update until task7

--- TestAPP.Test/UnitTest1.cs
+++ TestAPP.Test/UnitTest1.cs
@@ -10,37 +10,135 @@ namespace TestAPP.Test
         public void Setup()
         {
         }
+        [Test]
+        [Description(" returns EAST when passed NORTH and RIGHT")]
+
+        public void TestRotateNorthToRight()
+        {
+          // Arrange
+            Point inputPoint = Point.NORTH;
+            Direction inputDirection = Direction.RIGHT;
+            Compass compass = new Compass();
+
+           //Act
+           Point Result = compass.Rotate(inputPoint, inputDirection);
+
+
+            //Assert
+
+            Result.Should().Be(Point.EAST);
+        }
 
         [Test]
-        [TestCase(Point.NORTH, Direction.RIGHT, Point.EAST, TestName = "Test Roatate: North, right => EAST")]
-        [TestCase(Point.NORTH, Direction.LEFT, Point.WEST, TestName = "Test Roatate: North, left => WEST")]
-        [TestCase(Point.EAST, Direction.RIGHT, Point.SOUTH, TestName = "Test Roatate: East, right => SOUTH")]
-        [TestCase(Point.EAST, Direction.LEFT, Point.NORTH, TestName = "Test Roatate: East, left => NORTH")]
-        [TestCase(Point.SOUTH, Direction.RIGHT, Point.WEST, TestName = "Test Roatate: South, right => WEST")]
-        [TestCase(Point.SOUTH, Direction.LEFT, Point.EAST, TestName = "Test Roatate: South, left => EAST")]
-        [TestCase(Point.WEST, Direction.RIGHT, Point.NORTH, TestName = "Test Roatate: West, right => NORTH")]
-        [TestCase(Point.WEST, Direction.LEFT, Point.SOUTH, TestName = "Test Roatate: West, left => SOUTH")]
-
-
-        [Description(" returns EAST when passed NORTH and RIGHT")]
-        public void TestRotate(Point inputPoint, Direction inputDirection, Point expectedResult)
+       
+        public void TestRotateRight()
         {
             //Arrange
+            Point[] inputPoint = { Point.EAST, Point.WEST, Point.NORTH, Point.SOUTH };
+            Point[] expectedOutput = { Point.SOUTH, Point.NORTH, Point.EAST, Point.WEST };
+            Point[] result = new Point[4];
+
             Compass compass = new Compass();
 
             //Act
-            Point result = compass.Rotate(inputPoint, inputDirection);
+            for( int i=0; i<4; i++)
+            {
+               
+               result[i] = compass.Rotate(inputPoint[i], Direction.RIGHT);
+            }
+      
 
             //Assert
-            result.Should().Be(expectedResult);
+            //result.Should().Be(expectedResult);
 
             Assert.Multiple(() =>
             {
-                Assert.That(expectedResult , Is.EqualTo(result));
+                for (int i = 0; i < 4; ++i)
+                {
+                    Assert.That(expectedOutput[i], Is.EqualTo(result[i]));
+                }
             });
         }
+
+        [Test]
+        public void TestRotateLeft()
+        {
+            //Arrange
+            Point[] inputPoint = { Point.EAST, Point.WEST, Point.NORTH, Point.SOUTH };
+            Point[] expectedOutput = { Point.NORTH, Point.SOUTH, Point.WEST, Point.EAST };
+            Point[] result = new Point[4];
+
+            Compass compass = new Compass();
+
+            //Act
+            for (int i = 0; i < 4; i++)
+            {
+
+                result[i] = compass.Rotate(inputPoint[i], Direction.LEFT);
+            }
+
+
+            //Assert
+            //result.Should().Be(expectedResult);
+
+            Assert.Multiple(() =>
+            {
+                for (int i = 0; i < 4; ++i)
+                {
+                    Assert.That(expectedOutput[i], Is.EqualTo(result[i]));
+                }
+            });
+        }
+
+
        
 
+    }
+    public class TestStringManipulator
+    {
+
+        [Test]
+        [TestCase("Hello", "olleH", Description = "For Hello, Reverse String Returns olleH ?")]
+        [TestCase("", "", Description = "For empty string, Reverse String Returns empty string ?")]
+        [TestCase("**Hello!", "!olleH**", TestName = "For Hello, Reverse String Returns olleH ?")]
+        //      [TestCase(null, "Input can not be null", "For null, Reverse String Returns error message ?")]
+        public void TestReverseString(string inputString, string expectedResult)
+        {
+            //Arrange
+ 
+            StringManipulator stringManipulator = new StringManipulator();
+            string result;
+
+            //Assign
+            result = stringManipulator.ReverseString(inputString);
+
+
+            //Assert
+
+            result.Should().Be(expectedResult);
+        }
+        [Test]
+        [TestCase("madam", true, TestName = "madam is a Palindrom? true")]
+        [TestCase("Miss You!", false, TestName = "Miss You! is a Palindrom? false")]
+
+        public void TestIsPalindrome(string inputString, bool isPalindrome)
+        {
+            //Arrage
+            StringManipulator stringManipulator = new StringManipulator();
+            bool result;
+
+            //Assign
+            result = stringManipulator.IsPalindrome(inputString);
+
+            //Assert
+
+            result.Should().Be(isPalindrome);
+
+
+
+
+
+        }
 
     }
 }
---
+++ TestApp/StringManipulator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+ 
+        public class StringManipulator
+        {
+            public string ReverseString(string input)
+            {
+           
+              char[] inputArray = input.ToCharArray();
+              Array.Reverse(inputArray);            
+                return new string(inputArray);
+            }
+
+            public bool IsPalindrome(string input)
+            {
+              
+                return input == ReverseString(input);
+            }
+        }
+    
+}
